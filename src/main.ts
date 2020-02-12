0d999f4870a4ba4a9e1ec8bcae8dbdc75e565e27
Oleg <4uiiakabpa@gmail.com>
2020-02-13 00:24:30 +0200

    Add graceful shutdown handler

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+
+function terminationHandler(app: INestApplication) {
+  const shutdown = function () {
+    app.close().then(
+      () => {
+        process.exit();
+      },
+      (error) => {
+        console.error(error);
+        process.exitCode = 1;
+      }
+    );
+  }
+  process.on('SIGINT', function onSigint () {
+    console.info('Got SIGINT (aka ctrl-c in docker). Graceful shutdown ', new Date().toISOString());
+    shutdown();
+  });
+  process.on('SIGTERM', function onSigterm () {
+    console.info('Got SIGTERM (docker container stop). Graceful shutdown ', new Date().toISOString());
+    shutdown();
+  });
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app: INestApplication = await NestFactory.create(AppModule);
+  terminationHandler(app);
   await app.listen(3000);
 }
 bootstrap();
